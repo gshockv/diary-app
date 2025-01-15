337f1ba20ff7ca083bb27400f2988a9040f92c02
Vitaly Gashock <gshockv@gmail.com>
2025-01-15 17:39:24 +0200

    Splash screen

--- app/src/main/java/com/gshockv/dairyapp/MainActivity.kt
+++ app/src/main/java/com/gshockv/dairyapp/MainActivity.kt
@@ -4,14 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gshockv.dairyapp.ui.theme.DairyAppTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    installSplashScreen()
     enableEdgeToEdge()
     setContent {
       DairyAppTheme {
+        Surface(modifier = Modifier
+          .fillMaxSize()
+          .systemBarsPadding()
+        ) {
+          Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+          ) {
+            Text(
+              text = "Dairy App",
+              textAlign = TextAlign.Center,
+              fontWeight = FontWeight.Light
+            )
+          }
+        }
       }
     }
   }

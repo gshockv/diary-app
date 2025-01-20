dcfbd056aad04bb2ee394545800e1e09b7106d82
Vitaly Gashock <gshockv@gmail.com>
2025-01-20 23:00:08 +0200

    Edit  fields focus fixes

--- app/src/main/java/com/gshockv/dairyapp/ui/screen/write/WriteContent.kt
+++ app/src/main/java/com/gshockv/dairyapp/ui/screen/write/WriteContent.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
@@ -27,10 +29,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,11 +55,17 @@ fun WriteContent(
 ) {
   val context = LocalContext.current
   val scrollState = rememberScrollState()
+  val focusManager = LocalFocusManager.current
+
+  LaunchedEffect(scrollState.maxValue) {
+    scrollState.scrollTo(scrollState.maxValue)
+  }
 
   Column(
     modifier = modifier
       .fillMaxSize()
-      .padding(bottom = 24.dp)
+      .imePadding()
+      .navigationBarsPadding()
       .padding(horizontal = 24.dp),
     verticalArrangement = Arrangement.SpaceBetween
   ) {
@@ -102,7 +113,9 @@ fun WriteContent(
           imeAction = ImeAction.Next
         ),
         keyboardActions = KeyboardActions(
-          onNext = {}
+          onNext = {
+            focusManager.moveFocus(FocusDirection.Next)
+          }
         ),
         maxLines = 1,
         singleLine = true
@@ -128,7 +141,9 @@ fun WriteContent(
           imeAction = ImeAction.Next
         ),
         keyboardActions = KeyboardActions(
-          onNext = {}
+          onNext = {
+            focusManager.clearFocus()
+          }
         )
       )
     }
